80469b6ea9eb857b86b5c64bf2e63edd2df767d6
Luci <ignalucian@yahoo.com>
2019-08-31 19:19:18 +0300

    Overloading

--- src/main/java/module1/App.java
+++ src/main/java/module1/App.java
@@ -3,11 +3,21 @@ package module1;
 public class App {
     public static void main(String[] args) {
     Calcul1 calcul1= new Calcul1();
-    calcul1.getRezultatSuma(29.5,67.8);
+    calcul1.getRezultatSuma(34.4,67.7);
+        System.out.println("Rezultatul adunarii numerelor rationale este: "+calcul1.rezultatSuma);
+    calcul1.getRezultatSuma(34,56);
+        System.out.println("Rezultatul adunarii numerelor intregi este: "+calcul1.rezultatSuma);
     calcul1.getRezultatInmultire(13.3,12.7);
+        System.out.println("Rezultatul inmultirii numerelor rationale este: "+calcul1.rezultatInmultire);
+        calcul1.getRezultatInmultire(45,65);
+        System.out.println("Rezultatul inmultirii numerelor intregi este: "+calcul1.rezultatInmultire);
+    calcul1.getRezultatImpartire(23.8,7);
+        System.out.println("Rezultatul impartirii numerelor rationale este: "+calcul1.rezultatImpartire);
+    calcul1.getRezultatImpartire(45.0,8.0);
+        System.out.println("Rezultatul impartirii numerelor intregi este: "+calcul1.rezultatImpartire);
     calcul1.getRezultatPutere(2.9,4.8);
-        System.out.println("Rezultatul adunarii este: "+calcul1.rezultatSuma);
-        System.out.println("Rezultatul inmultirii este: "+calcul1.rezultatInmultire);
+        System.out.println("Rezultatul ridicarii la putere este: "+ calcul1.rezultatPutere);
+     calcul1.getRezultatPutere(9.0,7) ;
         System.out.println("Rezultatul ridicarii la putere este: "+ calcul1.rezultatPutere);
     }
 }
--- src/main/java/module1/Calcul1.java
+++ src/main/java/module1/Calcul1.java
@@ -3,8 +3,11 @@ package module1;
 public class Calcul1 {
     double x;
     double y;
+    int a;
+    int b;
     double rezultatSuma;
     double rezultatInmultire;
+    double rezultatImpartire;
     double rezultatPutere;
 
 
@@ -15,16 +18,44 @@ public class Calcul1 {
         return rezultatSuma = x + y;
     }
 
+    public double getRezultatSuma(int a, int b) {
+        this.a = a;
+        this.b = b;
+        return rezultatSuma = a + b;
+    }
+
     public double getRezultatInmultire(double x, double y) {
         this.x = x;
         this.y = y;
         return rezultatInmultire = x * y;
     }
 
+    public double getRezultatInmultire(int a, int b) {
+        this.a = a;
+        this.b = b;
+        return rezultatInmultire = a * b;
+    }
+
+    public double getRezultatImpartire(double x, double y) {
+        this.x = x;
+        this.y = y;
+        return rezultatImpartire = x / y;
+    }
+
+    public double getRezultatImpartire(int a, int b) {
+        this.a = a;
+        this.b = b;
+        return rezultatImpartire = a / b;
+    }
+
     public double getRezultatPutere(double x, double y) {
         this.x = x;
         this.y = y;
         return rezultatPutere = Math.pow(x, y);
-
+    }
+    public double getRezultatPutere(int a, int b){
+        this.a=a;
+        this.b=b;
+        return rezultatPutere=Math.pow(a,b);
     }
 }
